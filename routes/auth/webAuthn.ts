a8ed6c06b39f982ed54d0cb7b4d17cd65ecf2d34
Romil Tiwari <romil@obvious.technology>
2023-10-09 20:47:55 +0530

    fix: set rpID as assetlinks domain

--- routes/auth/webAuthn.ts
+++ routes/auth/webAuthn.ts
@@ -40,7 +40,7 @@ export function webAuthnGenerateRegistrationOptionsHandler(
 
 	// Get RP_ID from request Origin.
 	console.log("req.get('Origin')", req.get("Origin"));
-	const rpID = "com.hashhalli.obvious.money"; //getDomainFromUrl("localhost");
+	const rpID = "obvious-lit-relay.onrender.com"; //getDomainFromUrl("localhost");
 	//const rpID = getDomainFromUrl(req.get("Origin") || "localhost");
 
 	const authenticatorUsername = generateUsernameForOptions(username);
